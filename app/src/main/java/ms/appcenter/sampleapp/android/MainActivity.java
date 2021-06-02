300307ad9614b42a76951a72a62771d3f8bcdb33
Andrei Kamenev <akamenev@outlook.com>
2021-06-02 14:59:26 +0300

    Update MainActivity.java

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "4c4bfd1f-7ddb-4a76-8255-f65db36c416b",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
